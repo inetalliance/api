a8c8ac3cf6f86d0005f58f034fc2a4ca20edd5c2
Erik Rasmussen <rasmussenerik@gmail.com>
2019-06-11 18:18:03 +0200

    Referrer report

--- src/main/java/net/inetalliance/sonar/reporting/Referrals.java
+++ src/main/java/net/inetalliance/sonar/reporting/Referrals.java
@@ -4,8 +4,10 @@ import static net.inetalliance.types.www.ContentType.JSON;
 
 import com.callgrove.Callgrove;
 import com.callgrove.obj.Affiliate;
+import com.callgrove.obj.Opportunity;
+import com.callgrove.types.SaleSource;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.text.ParseException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,11 +18,11 @@ import net.inetalliance.funky.StringFun;
 import net.inetalliance.log.Log;
 import net.inetalliance.potion.Locator;
 import net.inetalliance.potion.cache.RedisJsonCache;
+import net.inetalliance.sql.OrderBy.Direction;
 import net.inetalliance.types.Currency;
 import net.inetalliance.types.json.JsonList;
 import net.inetalliance.types.json.JsonMap;
 import org.joda.time.DateMidnight;
-import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
 @WebServlet({"/reporting/reports/referrals"})
@@ -68,45 +70,12 @@ public class Referrals extends AngularServlet {
     final String cached = cache.get(q);
     if (StringFun.isEmpty(cached)) {
 
-      // ===============
-      // QUERY DATA HERE
-      // ===============
+      final JsonList phoneSales = query(interval, SaleSource.PHONE_CALL, affiliate);
+      final JsonList onlineOrders = query(interval, SaleSource.ONLINE, affiliate);
 
-      final JsonList phoneSales = Arrays.asList(
-          new JsonMap()
-              .$("id", 42)
-              .$("item", "Acme Widget 3000")
-              .$("date", new DateTime().minusDays(8))
-              .$("amount", new Currency(Math.random() * 200)),
-          new JsonMap()
-              .$("id", 45)
-              .$("item", "Awesome Product")
-              .$("date", new DateTime().minusDays(4))
-              .$("amount", new Currency(Math.random() * 200)),
-          new JsonMap()
-              .$("id", 48)
-              .$("item", "Turboencabulator 9000")
-              .$("date", new DateTime().minusDays(2))
-              .$("amount", new Currency(Math.random() * 200))
-      ).stream().collect(JsonList.collect);
-
-      final JsonList onlineOrders = Arrays.asList(
-          new JsonMap()
-              .$("id", 35)
-              .$("item", "100pc Sprocket Set")
-              .$("date", new DateTime().minusDays(9))
-              .$("amount", new Currency(Math.random() * 200)),
-          new JsonMap()
-              .$("id", 46)
-              .$("item", "Build your own product Kit")
-              .$("date", new DateTime().minusDays(6))
-              .$("amount", new Currency(Math.random() * 200)),
-          new JsonMap()
-              .$("id", 50)
-              .$("item", "Electric Washing Cabinet")
-              .$("date", new DateTime().minusDays(1))
-              .$("amount", new Currency(Math.random() * 200))
-      ).stream().collect(JsonList.collect);
+      // add commission
+      final Currency phoneCommissions = sumCommissions(phoneSales);
+      final Currency onlineCommissions = sumCommissions(onlineOrders);
 
       final int phoneLeadsCreated = (int) Math.floor(Math.random() * 200);
       final int phoneLeadsOpen = (int) Math.floor(Math.random() * phoneLeadsCreated);
@@ -120,7 +89,9 @@ public class Referrals extends AngularServlet {
           .$("phoneLeadsClosedAsDead", phoneLeadsClosedAsDead)
           .$("phoneLeadsOpen", phoneLeadsOpen)
           .$("phoneSales", phoneSales)
-          .$("onlineOrders", onlineOrders);
+          .$("phoneCommissions", phoneCommissions)
+          .$("onlineOrders", onlineOrders)
+          .$("onlineCommissions", onlineCommissions);
 
       cache.set(q, result);
       respond(response, result);
@@ -132,5 +103,38 @@ public class Referrals extends AngularServlet {
         writer.flush();
       }
     }
+  }
+
+  private static Currency sumCommissions(final JsonList list) {
+    return list
+        .stream()
+        .map(json -> ((JsonMap) json))
+        .map(json -> {
+          try {
+            return Currency.parse(json.get("commission"));
+          } catch (ParseException e) {
+            throw new RuntimeException(e);
+          }
+        })
+        .reduce(Currency.ZERO, Currency::add);
+  }
+
+  private static JsonList query(final Interval interval, final SaleSource source,
+      final Affiliate affiliate) {
+    return Locator
+        .$$(Opportunity.soldInInterval(interval)
+            .and(Opportunity.isSold)
+// TODO: uncomment this line to make this report actually work
+//            .and(Opportunity.withReferrer(affiliate.getDomain()))
+            .and(Opportunity.withSaleSource(source)
+                .orderBy("saleDate", Direction.ASCENDING)))
+        .stream()
+        .map(opp -> new JsonMap()
+            .$("id", opp.getId())
+            .$("item", opp.getProductLineName())
+            .$("date", opp.getSaleDate())
+            .$("amount", opp.getAmount())
+            .$("commission", opp.getAmount().multiply(affiliate.getCommission())))
+        .collect(JsonList.collect);
   }
 }
