b255caa34c924acaac9fa3f10a4b8c00e0bcd766
Erik Rasmussen <rasmussenerik@gmail.com>
2019-11-05 10:27:50 +0100

    Work on missed calls report

---
+++ src/main/java/net/inetalliance/sonar/reporting/MissedCallsByAgent.java
@@ -0,0 +1,174 @@
+package net.inetalliance.sonar.reporting;
+
+import com.callgrove.obj.Agent;
+import com.callgrove.obj.Call;
+import com.callgrove.obj.CallCenter;
+import com.callgrove.obj.ProductLine;
+import com.callgrove.obj.Site;
+import com.callgrove.types.ContactType;
+import com.callgrove.types.SaleSource;
+import net.inetalliance.angular.exception.BadRequestException;
+import net.inetalliance.angular.exception.UnauthorizedException;
+import net.inetalliance.funky.StringFun;
+import net.inetalliance.log.Log;
+import net.inetalliance.log.progress.ProgressMeter;
+import net.inetalliance.potion.Locator;
+import net.inetalliance.potion.info.Info;
+import net.inetalliance.potion.query.Query;
+import net.inetalliance.types.json.JsonList;
+import net.inetalliance.types.json.JsonMap;
+import org.joda.time.DateMidnight;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
+
+import static com.callgrove.Callgrove.*;
+import static java.util.stream.Collectors.*;
+import static net.inetalliance.log.Log.*;
+import static net.inetalliance.potion.Locator.*;
+
+@WebServlet({"/reporting/reports/missedCallsByAgent"})
+public class MissedCallsByAgent
+  extends CachedGroupingRangeReport<Agent, Site> {
+
+  private static final transient Log log = getInstance(MissedCallsByAgent.class);
+  private final Info<Site> info;
+
+  public MissedCallsByAgent() {
+    super("site", "productLine", "callCenter");
+    info = Info.$(Site.class);
+  }
+
+  @Override
+  protected String getGroupLabel(final Site group) {
+    return group.getName();
+  }
+
+  @Override
+  protected String getId(final Agent row) {
+    return row.key;
+  }
+
+  @Override
+  protected Query<Agent> allRows(final Set<Site> groups, final Agent loggedIn,
+                                 final DateTime intervalStart) {
+    return loggedIn.getViewableAgentsQuery(false).and(Agent.activeAfter(intervalStart))
+      .and(Agent.isSales);
+  }
+
+  @Override
+  protected Site getGroup(final String[] params, final String key) {
+    return info.lookup(key);
+  }
+
+  @Override
+  protected int getJobSize(final Agent loggedIn, final Set<Site> groups,
+                           final DateTime intervalStart) {
+    return count(allRows(groups, loggedIn, intervalStart));
+  }
+
+  @Override
+  protected JsonMap generate(final EnumSet<SaleSource> sources,
+                             final EnumSet<ContactType> contactTypes,
+                             final Agent loggedIn, final ProgressMeter meter,
+                             final DateMidnight start,
+                             final DateMidnight end,
+                             final Set<Site> sites, Collection<CallCenter> callCenters,
+                             final Map<String, String[]> extras) {
+    if (loggedIn == null || !(loggedIn.isManager() || loggedIn.isTeamLeader())) {
+      log.warning("%s tried to access closing report data",
+        loggedIn == null ? "Nobody?" : loggedIn.key);
+      throw new UnauthorizedException();
+    }
+    final String[] productLineIds = extras.get("productLine");
+    if (productLineIds.length == 0 || StringFun
+      .isEmpty(productLineIds[0])) {
+      throw new BadRequestException("Must specify product line via ?productLine=");
+    }
+    final Set<ProductLine> productLines = Arrays.stream(productLineIds)
+      .map(id -> Locator.$(new ProductLine(Integer.valueOf(id))))
+      .collect(toSet());
+    final Query<Call> productLinesQuery = productLines.isEmpty() ? Query.all(Call.class) :
+      Query.and(Call.class,
+        productLines.stream().map(Call::withProductLine).collect(toList()));
+
+    final Interval interval = getReportingInterval(start, end);
+
+    final Query<Call> query = Call.inInterval(interval)
+      .and(Call.withOpportunity())
+      .and(productLinesQuery);
+
+    final Set<Call> calls = $$(query);
+
+    final JsonList rows = new JsonList();
+//    final AtomicInteger totalCalls = new AtomicInteger(0);
+//    final AtomicInteger totalAgents = new AtomicInteger(0);
+//    final Map<Integer, AtomicInteger> callCenterCount = new HashMap<>();
+//    final Map<Integer, DailyPerformance> callCenterTotals = new HashMap<>();
+//    final Query<Opportunity> finalOppQuery = oppQuery;
+//    Locator.forEach(allRows(sites, loggedIn, interval.getStart()), agent -> {
+//      meter.increment(agent.getLastNameFirstInitial());
+//      final Query<Call> agentCallQuery = callQuery.and(Call.withBlame(agent));
+//      final DailyPerformance agentTotal = new DailyPerformance();
+//      Query<Call> queueCallCountQuery = callQuery.and(Call.withBlame(agent)).and(isQueue);
+//      if (!sources.isEmpty()) {
+//        queueCallCountQuery = queueCallCountQuery.and(Call.withSourceIn(sources));
+//      }
+//      agentTotal.setQueueCalls(
+//        uniqueCid ? countDistinct(queueCallCountQuery, "callerId_number")
+//          : count(queueCallCountQuery));
+//      final Query<Call> outboundQuery = callQuery.and(Call.withAgent(agent).and(isOutbound));
+//      if (uniqueCid) {
+//        agentTotal.setOutboundCalls(
+//          countDistinct(outboundQuery.join(Segment.class, "call"), "segment.callerid_number"));
+//
+//      } else {
+//        agentTotal.setOutboundCalls(count(outboundQuery));
+//      }
+//      agentTotal.setDumps(count(agentCallQuery.and(isQueue).and(Call.isDumped)));
+//      int closes = 0;
+//      Currency sales = Currency.ZERO;
+//      for (ProductLine productLine : productLines) {
+//
+//        final Query<Opportunity> agentOppQuery = finalOppQuery.and(withProductLine(productLine))
+//          .and(Opportunity.withAgent(agent))
+//          .and(withAmountGreaterThan(
+//            productLine.getLowestReasonableAmount()));
+//        closes += count(agentOppQuery);
+//        sales = sales.add($$(agentOppQuery, SUM, Currency.class, "amount"));
+//      }
+//      agentTotal.setCloses(closes);
+//      agentTotal.setSales(sales);
+//      if (agentTotal.getCloses() > 0 || agentTotal.getQueueCalls() > 0) {
+//        callCenterTotals.computeIfAbsent(agent.getCallCenter().id, k -> new DailyPerformance())
+//          .add(agentTotal);
+//        callCenterCount.computeIfAbsent(agent.getCallCenter().id, k -> new AtomicInteger(0))
+//          .incrementAndGet();
+//        totalAgents.incrementAndGet();
+//        totalCalls.addAndGet(agentTotal.getQueueCalls());
+//        rows.add(
+//          info.toJson(agentTotal).$("label", agent.getLastNameFirstInitial()).$("id", agent.key));
+//      }
+//
+//    });
+//    for (final CallCenter callCenter : loggedIn.getViewableCallCenters()) {
+//      final DailyPerformance callCenterTotal =
+//        callCenterTotals.computeIfAbsent(callCenter.id, k -> new DailyPerformance());
+//      if (callCenterTotal.getCloses() > 0 && callCenterTotal.getQueueCalls() > 0) {
+//        rows.add(info.toJson(callCenterTotal)
+//          .$("callCenter",
+//            callCenterCount.getOrDefault(callCenter.id, new AtomicInteger(0)).get())
+//          .$("label", callCenter.getName()));
+//      }
+//    }
+    return new JsonMap().$("rows", rows);
+//      .$("total", new JsonMap().$("agents", totalAgents.get()).$("queueCalls", totalCalls.get()));
+
+  }
+}
