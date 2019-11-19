670561b4de2f1e45f0e180f85846eeb6dcd4d201
Michael Ventura <michael.ventura@gmail.com>
2019-11-19 17:02:36 -0500

    fix missed call calculations

--- src/main/java/net/inetalliance/sonar/reporting/RevenueOverTime.java
+++ src/main/java/net/inetalliance/sonar/reporting/RevenueOverTime.java
@@ -1,11 +1,27 @@
 package net.inetalliance.sonar.reporting;
 
+import static net.inetalliance.types.www.ContentType.JSON;
+
 import com.callgrove.Callgrove;
 import com.callgrove.obj.Call;
 import com.callgrove.obj.Opportunity;
 import com.callgrove.obj.ProductLine;
 import com.callgrove.obj.Site;
 import com.callgrove.types.SaleSource;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import net.inetalliance.angular.AngularServlet;
 import net.inetalliance.funky.StringFun;
 import net.inetalliance.funky.math.Calculator;
@@ -22,26 +38,9 @@ import net.inetalliance.types.localized.LocalizedString;
 import org.joda.time.DateMidnight;
 import org.joda.time.Interval;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static net.inetalliance.types.www.ContentType.*;
-
 @WebServlet({"/reporting/reports/revenueOverTime"})
 public class RevenueOverTime
-  extends AngularServlet {
+    extends AngularServlet {
 
   private static final transient Log log = Log.getInstance(RevenueOverTime.class);
   private RedisJsonCache cache;
@@ -51,14 +50,14 @@ public class RevenueOverTime
 
   @Override
   public void init(final ServletConfig config)
-    throws ServletException {
+      throws ServletException {
     super.init(config);
     cache = new RedisJsonCache(getClass().getSimpleName());
   }
 
   @Override
   protected void get(HttpServletRequest request, HttpServletResponse response)
-    throws Exception {
+      throws Exception {
 
     final Interval interval = Callgrove.getInterval(request);
     final DateMidnight start = interval.getStart().toDateMidnight().withDayOfWeek(1);
@@ -67,57 +66,57 @@ public class RevenueOverTime
     final String[] siteStrings = request.getParameterValues("site");
     final String[] productLineStrings = request.getParameterValues("product");
 
-    Collection<Integer> sites = null;
-    Collection<ProductLine> productLines = null;
+    Collection<Integer> sites;
+    Collection<ProductLine> productLines;
 
     try {
       sites = siteStrings == null ? null : Arrays
-        .stream(siteStrings)
-        .map(Integer::parseInt)
-        .collect(Collectors.toSet());
+          .stream(siteStrings)
+          .map(Integer::parseInt)
+          .collect(Collectors.toSet());
     } catch (Exception e) {
       return;
     }
 
     try {
       productLines = productLineStrings == null ? null : Arrays
-        .stream(productLineStrings)
-        .map(Integer::parseInt)
-        .map(ProductLine::new)
-        .collect(Collectors.toSet());
+          .stream(productLineStrings)
+          .map(Integer::parseInt)
+          .map(ProductLine::new)
+          .collect(Collectors.toSet());
     } catch (Exception e) {
       return;
     }
 
     final String q =
-      String.format("report:%s,start:%s,end:%s,mode:%s,site:%s,product:%s",
-        getClass().getSimpleName(),
-        Callgrove.simple.print(start),
-        Callgrove.simple.print(end),
-        mode == null ? "" : String.join(",", mode),
-        sites == null ? null : sites
-          .stream()
-          .map(Object::toString)
-          .reduce((s1, s2) -> s1 + "|" + s2)
-          .get(),
-        productLines == null ? null : productLines
-          .stream()
-          .map(Object::toString)
-          .reduce((s1, s2) -> s1 + "|" + s2)
-          .get());
+        String.format("report:%s,start:%s,end:%s,mode:%s,site:%s,product:%s",
+            getClass().getSimpleName(),
+            Callgrove.simple.print(start),
+            Callgrove.simple.print(end),
+            mode == null ? "" : String.join(",", mode),
+            sites == null ? null : sites
+                .stream()
+                .map(Object::toString)
+                .reduce((s1, s2) -> s1 + "|" + s2)
+                .orElse(null),
+            productLines == null ? null : productLines
+                .stream()
+                .map(Object::toString)
+                .reduce((s1, s2) -> s1 + "|" + s2)
+                .orElse(null));
 
     final String cached = cache.get(q);
     if (StringFun.isEmpty(cached)) {
       final EnumSet<SaleSource> sources =
-        mode == null || mode.length == 0 || (mode.length == 1 && "all".equals(mode[0]))
-          ? EnumSet.noneOf(SaleSource.class)
-          : Arrays.stream(mode)
-          .map(s -> StringFun.camelCaseToEnum(SaleSource.class, s))
-          .collect(Collectors.toCollection(() -> EnumSet.noneOf(SaleSource.class)));
+          mode == null || mode.length == 0 || (mode.length == 1 && "all".equals(mode[0]))
+              ? EnumSet.noneOf(SaleSource.class)
+              : Arrays.stream(mode)
+                  .map(s -> StringFun.camelCaseToEnum(SaleSource.class, s))
+                  .collect(Collectors.toCollection(() -> EnumSet.noneOf(SaleSource.class)));
 
       Query<Opportunity> query =
-        Opportunity.isSold
-          .and(Opportunity.soldInInterval(new Interval(start.minusWeeks(8), end)));
+          Opportunity.isSold
+              .and(Opportunity.soldInInterval(new Interval(start.minusWeeks(8), end)));
       Query<Call> callQuery = Query.all(Call.class);
 
       if (!sources.isEmpty()) {
@@ -140,25 +139,26 @@ public class RevenueOverTime
       for (DateMidnight date = start.minusWeeks(8); !date.isAfter(end); date = date.plusWeeks(1)) {
         final Interval week = new Interval(date, date.plusWeeks(1));
         rows.add(
-          new JsonMap()
-            .$("date", date)
-            .$("missedCalls", Locator.count(callQuery.and(Call.inInterval(week))))
-            .$("calls", Locator.count(callQuery.and(Call.missed).and(Call.inInterval(week))))
-            .$("revenue",
-              opportunities
-                .stream()
-                .filter(Opportunity.soldInInterval(week))
-                .map(Opportunity::getAmount)
-                .reduce(Currency.ZERO, (amount, total) -> amount.add(total))
-                .doubleValue()));
+            new JsonMap()
+                .$("date", date)
+                .$("missedCalls",
+                    Locator.count(Call.isAnswered.negate().and(Call.inInterval(week))))
+                .$("calls", Locator.count(callQuery.and(Call.inInterval(week))))
+                .$("revenue",
+                    opportunities
+                        .stream()
+                        .filter(Opportunity.soldInInterval(week))
+                        .map(Opportunity::getAmount)
+                        .reduce(Currency.ZERO, Currency::add)
+                        .doubleValue()));
       }
 
       // calculate averages
       final List<Double> lastTwo = new ArrayList<>(2);
       final List<Double> lastFour = new ArrayList<>(4);
       final List<Double> lastEight = new ArrayList<>(8);
-      for (int i = 0; i < rows.size(); i++) {
-        JsonMap row = (JsonMap) rows.get(i);
+      for (Json json : rows) {
+        JsonMap row = (JsonMap) json;
         final Double revenue = row.getDouble("revenue");
         lastTwo.add(revenue);
         lastFour.add(revenue);
@@ -198,41 +198,41 @@ public class RevenueOverTime
       }
 
       final Collection<Query<? super ProductLine>> productLineQueries =
-        productLines == null ? null : productLines
-          .stream()
-          .map(ProductLine::getId)
-          .map(ProductLine::withId)
-          .collect(Collectors.toList());
+          productLines == null ? null : productLines
+              .stream()
+              .map(ProductLine::getId)
+              .map(ProductLine::withId)
+              .collect(Collectors.toList());
       final JsonList productLineNames = productLineQueries == null ? null
-        : Locator.$$(Query.or(ProductLine.class, productLineQueries))
-        .stream()
-        .map(ProductLine::getName)
-        .map(JsonString::new)
-        .collect(JsonList.collect);
+          : Locator.$$(Query.or(ProductLine.class, productLineQueries))
+              .stream()
+              .map(ProductLine::getName)
+              .map(JsonString::new)
+              .collect(JsonList.collect);
 
       final Collection<Query<? super Site>> siteQueries =
-        sites == null ? null : sites
-          .stream()
-          .map(Site::withId)
-          .collect(Collectors.toList());
+          sites == null ? null : sites
+              .stream()
+              .map(Site::withId)
+              .collect(Collectors.toList());
       final JsonList siteAbbreviations = siteQueries == null ? null
-        : Locator.$$(Query.or(Site.class, siteQueries))
-        .stream()
-        .map(Site::getAbbreviation)
-        .map(Object::toString)
-        .map(JsonString::new)
-        .collect(JsonList.collect);
+          : Locator.$$(Query.or(Site.class, siteQueries))
+              .stream()
+              .map(Site::getAbbreviation)
+              .map(Object::toString)
+              .map(JsonString::new)
+              .collect(JsonList.collect);
 
       final JsonMap result = new JsonMap()
-        .$("rows", rows)
-        .$("sources", sources == null ? null : sources
-          .stream()
-          .map(SaleSource::getLocalizedName)
-          .map(LocalizedString::toString)
-          .map(JsonString::new)
-          .collect(JsonList.collect))
-        .$("productLines", productLineNames == null ? null : productLineNames)
-        .$("sites", siteAbbreviations == null ? null : siteAbbreviations);
+          .$("rows", rows)
+          .$("sources", sources
+                        .stream()
+                        .map(SaleSource::getLocalizedName)
+                        .map(LocalizedString::toString)
+                        .map(JsonString::new)
+                        .collect(JsonList.collect))
+          .$("productLines", productLineNames)
+          .$("sites", siteAbbreviations);
 
       cache.set(q, result);
       respond(response, result);
