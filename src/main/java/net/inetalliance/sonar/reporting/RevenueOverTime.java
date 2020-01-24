370450cb1dbf2389504c9d6ca33761739d12dee9
Erik Rasmussen <rasmussenerik@gmail.com>
2020-01-24 09:27:12 +0100

    Used Call.missed query in Revenue Over Time report

--- src/main/java/net/inetalliance/sonar/reporting/RevenueOverTime.java
+++ src/main/java/net/inetalliance/sonar/reporting/RevenueOverTime.java
@@ -1,9 +1,5 @@
 package net.inetalliance.sonar.reporting;
 
-import static java.util.stream.Collectors.toSet;
-import static net.inetalliance.sonar.reporting.ProductLineClosing.getQueues;
-import static net.inetalliance.types.www.ContentType.JSON;
-
 import com.callgrove.Callgrove;
 import com.callgrove.obj.Agent;
 import com.callgrove.obj.Call;
@@ -11,20 +7,6 @@ import com.callgrove.obj.Opportunity;
 import com.callgrove.obj.ProductLine;
 import com.callgrove.obj.Site;
 import com.callgrove.types.SaleSource;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import net.inetalliance.angular.AngularServlet;
 import net.inetalliance.angular.exception.ForbiddenException;
 import net.inetalliance.funky.Funky;
@@ -44,9 +26,28 @@ import net.inetalliance.util.security.auth.Authorized;
 import org.joda.time.DateMidnight;
 import org.joda.time.Interval;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static net.inetalliance.sonar.reporting.ProductLineClosing.*;
+import static net.inetalliance.types.www.ContentType.*;
+
 @WebServlet({"/reporting/reports/revenueOverTime"})
 public class RevenueOverTime
-    extends AngularServlet {
+  extends AngularServlet {
 
   private static final transient Log log = Log.getInstance(RevenueOverTime.class);
   private RedisJsonCache cache;
@@ -56,14 +57,14 @@ public class RevenueOverTime
 
   @Override
   public void init(final ServletConfig config)
-      throws ServletException {
+    throws ServletException {
     super.init(config);
     cache = new RedisJsonCache(getClass().getSimpleName());
   }
 
   @Override
   protected void get(HttpServletRequest request, HttpServletResponse response)
-      throws Exception {
+    throws Exception {
     final Authorized authorized = Auth.getAuthorized(request);
     if (authorized == null || !authorized.isAuthorized("reports")) {
       throw new ForbiddenException("You do not have access to reports");
@@ -81,52 +82,52 @@ public class RevenueOverTime
 
     try {
       sites = siteStrings == null ? null : Arrays
-          .stream(siteStrings)
-          .map(Integer::parseInt)
-          .collect(Collectors.toSet());
+        .stream(siteStrings)
+        .map(Integer::parseInt)
+        .collect(Collectors.toSet());
     } catch (Exception e) {
       return;
     }
 
     try {
       productLines = productLineStrings == null ? null : Arrays
-          .stream(productLineStrings)
-          .map(Integer::parseInt)
-          .map(ProductLine::new)
-          .collect(Collectors.toSet());
+        .stream(productLineStrings)
+        .map(Integer::parseInt)
+        .map(ProductLine::new)
+        .collect(Collectors.toSet());
     } catch (Exception e) {
       return;
     }
 
     final String q =
-        String.format("report:%s,start:%s,end:%s,mode:%s,site:%s,product:%s",
-            getClass().getSimpleName(),
-            Callgrove.simple.print(start),
-            Callgrove.simple.print(end),
-            mode == null ? "" : String.join(",", mode),
-            sites == null ? null : sites
-                .stream()
-                .map(Object::toString)
-                .reduce((s1, s2) -> s1 + "|" + s2)
-                .orElse(null),
-            productLines == null ? null : productLines
-                .stream()
-                .map(Object::toString)
-                .reduce((s1, s2) -> s1 + "|" + s2)
-                .orElse(null));
+      String.format("report:%s,start:%s,end:%s,mode:%s,site:%s,product:%s",
+        getClass().getSimpleName(),
+        Callgrove.simple.print(start),
+        Callgrove.simple.print(end),
+        mode == null ? "" : String.join(",", mode),
+        sites == null ? null : sites
+          .stream()
+          .map(Object::toString)
+          .reduce((s1, s2) -> s1 + "|" + s2)
+          .orElse(null),
+        productLines == null ? null : productLines
+          .stream()
+          .map(Object::toString)
+          .reduce((s1, s2) -> s1 + "|" + s2)
+          .orElse(null));
 
     final String cached = cache.get(q);
     if (StringFun.isEmpty(cached)) {
       final EnumSet<SaleSource> sources =
-          mode == null || mode.length == 0 || (mode.length == 1 && "all".equals(mode[0]))
-              ? EnumSet.noneOf(SaleSource.class)
-              : Arrays.stream(mode)
-                  .map(s -> StringFun.camelCaseToEnum(SaleSource.class, s))
-                  .collect(Collectors.toCollection(() -> EnumSet.noneOf(SaleSource.class)));
+        mode == null || mode.length == 0 || (mode.length == 1 && "all".equals(mode[0]))
+          ? EnumSet.noneOf(SaleSource.class)
+          : Arrays.stream(mode)
+          .map(s -> StringFun.camelCaseToEnum(SaleSource.class, s))
+          .collect(Collectors.toCollection(() -> EnumSet.noneOf(SaleSource.class)));
 
       Query<Opportunity> query =
-          Opportunity.isSold
-              .and(Opportunity.soldInInterval(new Interval(start.minusWeeks(8), end)));
+        Opportunity.isSold
+          .and(Opportunity.soldInInterval(new Interval(start.minusWeeks(8), end)));
       Query<Call> callQuery = Call.isQueue;
 
       if (!sources.isEmpty()) {
@@ -142,35 +143,37 @@ public class RevenueOverTime
         query = query.and(Opportunity.withProductLineIn(productLines));
 
         final Set<Site> siteObjects = sites == null ? null : sites.stream()
-            .map(id -> Locator.$(new Site(id))).collect(toSet());
+          .map(id -> Locator.$(new Site(id))).collect(toSet());
         callQuery = callQuery.and(Call.withQueueIn(
-            productLines.stream()
-                .map(pl -> getQueues(loggedIn, pl, siteObjects))
-                .flatMap(Funky::stream)
-                .collect(toSet())));
+          productLines.stream()
+            .map(pl -> getQueues(loggedIn, pl, siteObjects))
+            .flatMap(Funky::stream)
+            .collect(toSet())));
       }
       final Set<Opportunity> opportunities = Locator.$$(query);
       final JsonList rows = new JsonList();
 
       // we start back 8 weeks to get the moving average for our start date
       for (DateMidnight date = start.minusWeeks(8); !date.plusWeeks(1).isAfter(end);
-          date = date.plusWeeks(1)) {
+        date = date.plusWeeks(1)) {
         final Interval week = new Interval(date, date.plusWeeks(1));
         rows.add(
-            new JsonMap()
-                .$("date", date)
-                .$("missedCalls",
-                    Locator
-                        .count(callQuery.and(Call.isBusinessHours)
-                            .and(Call.isAnswered.negate()).and(Call.inInterval(week))))
-                .$("calls", Locator.count(callQuery.and(Call.inInterval(week))))
-                .$("revenue",
-                    opportunities
-                        .stream()
-                        .filter(Opportunity.soldInInterval(week))
-                        .map(Opportunity::getAmount)
-                        .reduce(Currency.ZERO, Currency::add)
-                        .doubleValue()));
+          new JsonMap()
+            .$("date", date)
+            .$("missedCalls",
+              Locator
+                .count(callQuery
+                  .and(Call.missed)
+                  .and(Call.isBusinessHours)
+                  .and(Call.inInterval(week))))
+            .$("calls", Locator.count(callQuery.and(Call.inInterval(week))))
+            .$("revenue",
+              opportunities
+                .stream()
+                .filter(Opportunity.soldInInterval(week))
+                .map(Opportunity::getAmount)
+                .reduce(Currency.ZERO, Currency::add)
+                .doubleValue()));
       }
 
       // calculate averages
@@ -218,41 +221,41 @@ public class RevenueOverTime
       }
 
       final Collection<Query<? super ProductLine>> productLineQueries =
-          productLines == null ? null : productLines
-              .stream()
-              .map(ProductLine::getId)
-              .map(ProductLine::withId)
-              .collect(Collectors.toList());
+        productLines == null ? null : productLines
+          .stream()
+          .map(ProductLine::getId)
+          .map(ProductLine::withId)
+          .collect(Collectors.toList());
       final JsonList productLineNames = productLineQueries == null ? null
-          : Locator.$$(Query.or(ProductLine.class, productLineQueries))
-              .stream()
-              .map(ProductLine::getName)
-              .map(JsonString::new)
-              .collect(JsonList.collect);
+        : Locator.$$(Query.or(ProductLine.class, productLineQueries))
+        .stream()
+        .map(ProductLine::getName)
+        .map(JsonString::new)
+        .collect(JsonList.collect);
 
       final Collection<Query<? super Site>> siteQueries =
-          sites == null ? null : sites
-              .stream()
-              .map(Site::withId)
-              .collect(Collectors.toList());
+        sites == null ? null : sites
+          .stream()
+          .map(Site::withId)
+          .collect(Collectors.toList());
       final JsonList siteAbbreviations = siteQueries == null ? null
-          : Locator.$$(Query.or(Site.class, siteQueries))
-              .stream()
-              .map(Site::getAbbreviation)
-              .map(Object::toString)
-              .map(JsonString::new)
-              .collect(JsonList.collect);
+        : Locator.$$(Query.or(Site.class, siteQueries))
+        .stream()
+        .map(Site::getAbbreviation)
+        .map(Object::toString)
+        .map(JsonString::new)
+        .collect(JsonList.collect);
 
       final JsonMap result = new JsonMap()
-          .$("rows", rows)
-          .$("sources", sources
-              .stream()
-              .map(SaleSource::getLocalizedName)
-              .map(LocalizedString::toString)
-              .map(JsonString::new)
-              .collect(JsonList.collect))
-          .$("productLines", productLineNames)
-          .$("sites", siteAbbreviations);
+        .$("rows", rows)
+        .$("sources", sources
+          .stream()
+          .map(SaleSource::getLocalizedName)
+          .map(LocalizedString::toString)
+          .map(JsonString::new)
+          .collect(JsonList.collect))
+        .$("productLines", productLineNames)
+        .$("sites", siteAbbreviations);
 
       cache.set(q, result);
       respond(response, result);
