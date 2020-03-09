a3927d2facb2c4fa0cfffab859110f494a1e9cb5
Michael Ventura <michael.ventura@gmail.com>
2020-03-09 11:33:44 -0400

    restrict access to skillroute editor. remove mobile

--- src/main/java/net/inetalliance/sonar/api/SkillRoutes.java
+++ src/main/java/net/inetalliance/sonar/api/SkillRoutes.java
@@ -1,11 +1,15 @@
 package net.inetalliance.sonar.api;
 
 import com.callgrove.obj.Agent;
+import com.callgrove.obj.Site;
 import com.callgrove.obj.SkillRoute;
 import com.callgrove.obj.SkilledAgent;
 import com.callgrove.types.Tier;
 import net.inetalliance.angular.Key;
 import net.inetalliance.angular.exception.BadRequestException;
+import net.inetalliance.angular.exception.ForbiddenException;
+import net.inetalliance.funky.Funky;
+import net.inetalliance.log.Log;
 import net.inetalliance.potion.Locator;
 import net.inetalliance.potion.query.Query;
 import net.inetalliance.sonar.ListableModel;
@@ -16,85 +20,120 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
+import static com.callgrove.types.Tier.MOBILE;
+import static java.util.stream.Collectors.toSet;
 import static net.inetalliance.funky.StringFun.*;
 import static net.inetalliance.potion.Locator.*;
-import static net.inetalliance.sql.OrderBy.Direction.*;
 
 @WebServlet("/api/skillRoute/*")
 public class SkillRoutes
-  extends ListableModel.Named<SkillRoute> {
+        extends ListableModel.Named<SkillRoute> {
 
-  public SkillRoutes() {
-    super(SkillRoute.class);
-  }
+    public SkillRoutes() {
+        super(SkillRoute.class);
+    }
 
-  @Override
-  public Query<SkillRoute> all(final Class<SkillRoute> type, final HttpServletRequest request) {
-    return super.all(type, request).orderBy("name", ASCENDING);
-  }
 
-  @Override
-  protected void get(final HttpServletRequest request, final HttpServletResponse response)
-    throws Exception {
-    final Key<SkillRoute> key = getKey(request);
-    if (!isEmpty(key.id)) {
-      final SkillRoute route = lookup(key, request);
-      if (route != null) {
+    private static final Set<String> allowedEditors = Set.of("7000", "7001", "7006", "7007");
 
-        final JsonMap result = new JsonMap();
-        final Map<Tier, Collection<Agent>> members = route.getConfiguredMembers();
-        for (final Tier tier : Tier.values()) {
-          result.$(tier.name(),
-            members.containsKey(tier) ? members.get(tier).stream().map(agent -> new JsonMap()
-              .$("name",
-                agent.getLastNameFirstInitial()).$("key", agent.key)).collect(JsonList.collect) :
-              JsonList.empty);
+        private static final Function<Agent,Set<Integer>> visibleRoutes = Funky.memoize(10,agent ->
+            agent.getVisibleSites().stream()
+                        .map(s -> Locator.$$(Site.SiteQueue.withSite(s)))
+            .flatMap(Funky::stream)
+                        .distinct()
+                        .map(sq -> sq.queue.getSkillRoute().id).collect(toSet()));
+
+    @Override
+    public Query<SkillRoute> all(final Class<SkillRoute> type, final HttpServletRequest request) {
+        var loggedIn = Startup.getAgent(request);
+        if (loggedIn == null || !allowedEditors.contains(loggedIn.key)) {
+            throw new ForbiddenException();
         }
-        respond(response, result);
-        return;
-      }
-    }
-    super.get(request, response);
-  }
-
-  @Override
-  protected void post(final HttpServletRequest request, final HttpServletResponse response)
-    throws Exception {
-    final Key<SkillRoute> key = getKey(request);
-    if (isEmpty(key.id)) {
-      throw new BadRequestException("No key");
-    }
-    final SkillRoute route = lookup(key, request);
-    if (route == null) {
-      throw new BadRequestException("No route");
-    }
-    final JsonMap json = JsonMap.parse(request.getInputStream());
-    final Tier tier = json.getEnum("tier", Tier.class);
-    final String agent = json.get("agent");
-    if (tier == null) {
-      throw new BadRequestException("No tier");
-    }
-    if (agent == null) {
-      throw new BadRequestException("No agent");
+        return Query.in(SkillRoute.class,"id",visibleRoutes.apply(loggedIn));
     }
 
-    final SkilledAgent skilledAgent = $1(SkilledAgent.withRoute(route).and(
-      SkilledAgent.withAgent(new Agent(agent))));
-    if (skilledAgent == null) {
-      // No skilled agent record. Create one.
-      final SkilledAgent newSkilledAgent = new SkilledAgent();
-      newSkilledAgent.skillRoute = route;
-      newSkilledAgent.setAgent(new Agent(agent));
-      newSkilledAgent.setTier(tier);
-      Locator.create(getRemoteUser(request), newSkilledAgent);
-    } else {
-      Locator.update(skilledAgent, getRemoteUser(request),
-        copy -> {
-          copy.setTier(tier);
-        });
+    @Override
+    protected void get(final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
+        final Key<SkillRoute> key = getKey(request);
+        if (!isEmpty(key.id)) {
+            var loggedIn = Startup.getAgent(request);
+            if(loggedIn == null || !visibleRoutes.apply(loggedIn).contains(Integer.valueOf(key.id))) {
+                if(loggedIn != null) {
+                    log.warning("%s tried to access the skill route editor for %d", loggedIn.getFullName(), key.id);
+                }
+               throw new ForbiddenException();
+            }
+            final SkillRoute route = lookup(key, request);
+            if (route != null) {
+
+                final JsonMap result = new JsonMap();
+                final Map<Tier, Collection<Agent>> members = route.getConfiguredMembers();
+                for (final Tier tier : EnumSet.complementOf(EnumSet.of(MOBILE))) {
+                    result.$(tier.name(),
+                            members.containsKey(tier) ? members.get(tier).stream().map(agent -> new JsonMap()
+                                    .$("name",
+                                            agent.getLastNameFirstInitial()).$("key", agent.key)).collect(JsonList.collect) :
+                                    JsonList.empty);
+                }
+                respond(response, result);
+                return;
+            }
+        }
+        super.get(request, response);
     }
-    respond(response, new JsonMap().$("success", true));
-  }
+
+    @Override
+    protected void post(final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
+        final Key<SkillRoute> key = getKey(request);
+        if (isEmpty(key.id)) {
+            throw new BadRequestException("No key");
+        }
+        final SkillRoute route = lookup(key, request);
+        if (route == null) {
+            throw new BadRequestException("No route");
+        }
+        var loggedIn = Startup.getAgent(request);
+        if(loggedIn == null || !visibleRoutes.apply(loggedIn).contains(route.id)) {
+            if(loggedIn != null) {
+                log.warning("%s tried to access the skill route editor for %d",
+                        loggedIn.getFullName(), key.id);
+            }
+            throw new ForbiddenException();
+        }
+        final JsonMap json = JsonMap.parse(request.getInputStream());
+        final Tier tier = json.getEnum("tier", Tier.class);
+        final String agent = json.get("agent");
+        if (tier == null) {
+            throw new BadRequestException("No tier");
+        }
+        if (agent == null) {
+            throw new BadRequestException("No agent");
+        }
+
+        final SkilledAgent skilledAgent = $1(SkilledAgent.withRoute(route).and(
+                SkilledAgent.withAgent(new Agent(agent))));
+        if (skilledAgent == null) {
+            // No skilled agent record. Create one.
+            final SkilledAgent newSkilledAgent = new SkilledAgent();
+            newSkilledAgent.skillRoute = route;
+            newSkilledAgent.setAgent(new Agent(agent));
+            newSkilledAgent.setTier(tier);
+            Locator.create(getRemoteUser(request), newSkilledAgent);
+        } else {
+            Locator.update(skilledAgent, getRemoteUser(request),
+                    copy -> {
+                        copy.setTier(tier);
+                    });
+        }
+        respond(response, new JsonMap().$("success", true));
+    }
+
+    private static final Log log = Log.getInstance(SkillRoutes.class);
 }
