56df11dd75cfe52c497e464179fcadfb55a7f989
Michael Ventura <michael.ventura@gmail.com>
2020-10-09 11:29:49 -0400

    update deprecated usage

--- src/main/java/net/inetalliance/sonar/YouTrack.java
+++ src/main/java/net/inetalliance/sonar/YouTrack.java
@@ -5,12 +5,13 @@ import net.inetalliance.log.Log;
 import net.inetalliance.types.json.JsonMap;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
@@ -23,65 +24,73 @@ import static net.inetalliance.types.json.Json.pretty;
 
 public class YouTrack {
 
-  public static final String api = "https://youtrack.inetalliance.net/rest";
-  private static final transient Log log = getInstance(YouTrack.class);
-  private HttpClient client;
+    public static final String api = "https://youtrack.inetalliance.net/rest";
+    private static final transient Log log = getInstance(YouTrack.class);
+    private final PoolingHttpClientConnectionManager cxnManager;
+    private final HttpClientBuilder clientBuilder;
 
-  public YouTrack() {
-    super();
-    client = new DefaultHttpClient(new PoolingClientConnectionManager());
-  }
 
-  public static void main(final String[] args) {
-    try {
-      final YouTrack youTrack = new YouTrack();
-      final JsonMap jsonMap =
-          youTrack.get(
-              "/issue?filter=project%3A%7BAmeriGlide+Customer+Service%7D+Opportunity+ID%3A+227433");
-      //%7D+Opportunity+ID" +
-      //"%3A+" + 227433 );+ "&with=id&max=1024");
-      System.out.println(pretty(jsonMap));
-      youTrack.shutdown();
-    } catch (Throwable t) {
-      log.error(t);
+    public YouTrack() {
+        super();
+        cxnManager = new PoolingHttpClientConnectionManager();
+        this.clientBuilder = HttpClientBuilder.create()
+                .setDefaultRequestConfig(
+                        RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build())
+                .setConnectionManager(cxnManager);
     }
-  }
 
-  public synchronized JsonMap get(final String path)
-      throws IOException {
-    final HttpGet request = new HttpGet(api + path);
-    request.addHeader("Accept", "application/json");
-    log.debug("[YouTrack] requesting " + path);
-    final HttpResponse response = client.execute(request);
-    final int code = response.getStatusLine().getStatusCode();
-    switch (code) {
-      case 200:
-        log.info("[YouTrack] Got response");
-        return JsonMap.parse(response.getEntity().getContent());
-      case 401:
-        EntityUtils.consumeQuietly(response.getEntity());
-        log.info("[YouTrack] Auth required, logging in");
-        final HttpPost login = new HttpPost(api + "/user/login");
-        final List<NameValuePair> data = new ArrayList<>(2);
-        data.add(new BasicNameValuePair("login", "api"));
-        data.add(new BasicNameValuePair("password", "4sa7ya,o"));
-        login.setEntity(new UrlEncodedFormEntity(data));
-        final HttpResponse authResponse = client.execute(login);
-        if (authResponse.getStatusLine().getStatusCode() == 200) {
-          log.info("[YouTrack] Logged in successfully as api");
-          EntityUtils.consumeQuietly(authResponse.getEntity());
-          return get(path);
+    public static void main(final String[] args) {
+        try {
+            final YouTrack youTrack = new YouTrack();
+            final JsonMap jsonMap =
+                    youTrack.get(
+                            "/issue?filter=project%3A%7BAmeriGlide+Customer+Service%7D+Opportunity+ID%3A+227433");
+            //%7D+Opportunity+ID" +
+            //"%3A+" + 227433 );+ "&with=id&max=1024");
+            System.out.println(pretty(jsonMap));
+            youTrack.shutdown();
+        } catch (Throwable t) {
+            log.error(t);
         }
-        throw new BadRequestException("Could not login to youtrack: %s",
-            authResponse.getStatusLine().getReasonPhrase());
-      default:
-        throw new BadRequestException("Got back:", response.getStatusLine().getReasonPhrase());
+    }
+
+    public synchronized JsonMap get(final String path)
+            throws IOException {
+        final HttpGet request = new HttpGet(api + path);
+        request.addHeader("Accept", "application/json");
+        log.debug("[YouTrack] requesting " + path);
+        try (var client = clientBuilder.build()) {
+            final HttpResponse response = client.execute(request);
+            final int code = response.getStatusLine().getStatusCode();
+            switch (code) {
+                case 200:
+                    log.info("[YouTrack] Got response");
+                    return JsonMap.parse(response.getEntity().getContent());
+                case 401:
+                    EntityUtils.consumeQuietly(response.getEntity());
+                    log.info("[YouTrack] Auth required, logging in");
+                    final HttpPost login = new HttpPost(api + "/user/login");
+                    final List<NameValuePair> data = new ArrayList<>(2);
+                    data.add(new BasicNameValuePair("login", "api"));
+                    data.add(new BasicNameValuePair("password", "4sa7ya,o"));
+                    login.setEntity(new UrlEncodedFormEntity(data));
+                    final HttpResponse authResponse = client.execute(login);
+                    if (authResponse.getStatusLine().getStatusCode() == 200) {
+                        log.info("[YouTrack] Logged in successfully as api");
+                        EntityUtils.consumeQuietly(authResponse.getEntity());
+                        return get(path);
+                    }
+                    throw new BadRequestException("Could not login to youtrack: %s",
+                            authResponse.getStatusLine().getReasonPhrase());
+                default:
+                    throw new BadRequestException("Got back:", response.getStatusLine().getReasonPhrase());
+
+            }
+        }
 
     }
 
-  }
-
-  public void shutdown() {
-    client.getConnectionManager().shutdown();
-  }
+    public void shutdown() {
+        cxnManager.shutdown();
+    }
 }
